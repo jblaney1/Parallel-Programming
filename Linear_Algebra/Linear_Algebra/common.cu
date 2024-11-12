bb204d24591a3f0e9ccb0e7cbaa26eb6132c617d
jblaney1 <35744874+jblaney1@users.noreply.github.com>
2024-11-12 12:53:38 -0700

    Add files via upload

---
+++ Linear_Algebra/Linear_Algebra/common.cu
@@ -0,0 +1,28 @@
+/*
+*
+*/
+
+
+#include "common.cuh"
+
+
+bool HandleCUDAError(cudaError_t t)
+{
+	if (t != cudaSuccess)
+	{
+		cout << cudaGetErrorString(cudaGetLastError());
+		return false;
+	}
+	return true;
+}
+
+bool GetCUDARunTimeError()
+{
+	cudaError_t t = cudaGetLastError();
+	if (t != cudaSuccess)
+	{
+		cout << cudaGetErrorString(t) << endl;
+		return false;
+	}
+	return true;
+}
